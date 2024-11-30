89981cec828ea7656ad5c1d293cd78b8bcd434e0
Fe-56 <limfuoen1999@gmail.com>
2024-11-30 11:16:46 +0800

    Additional optimizations, and add in logic to aid in notifying users on frontend of certain events

--- backend/contracts/Campaign.sol
+++ backend/contracts/Campaign.sol
@@ -2,7 +2,7 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
-/** 
+/**
  * @title Campaign
  * @dev The smart contract for the created Campaign. It handles everything related to a specific Campaign that has been deployed
  */
@@ -11,14 +11,14 @@ contract Campaign {
     address public immutable beneficiary;
     string public name;
     string public description;
-    uint public immutable deadline;
+    uint256 public immutable deadline;
     uint32 public immutable fundingGoal;
     uint32 public totalFunds; // the amount raised
     bool public isActive; // if true, then the Campaign is ongoing, donations are allowed, and is not finalized; else, the Campaign has ended, and no further actions ar allowed
     mapping(address => uint32) public donations;
-    address[] public donors; // array to store donor addresses for refunds if needed.
+    address[] public donors; // array to store donor addresses for refunds
 
-    /** 
+    /**
      * @dev Creates the Campaign smart contract
      * @param _beneficiary The wallet address of the beneficiary that is creating this Campaign
      * @param _description The description of this Campaign
@@ -30,7 +30,7 @@ contract Campaign {
         string memory _name,
         string memory _description,
         uint32 _fundingGoal,
-        uint _deadline
+        uint256 _deadline
     ) {
         beneficiary = _beneficiary;
         name = _name;
@@ -39,25 +39,36 @@ contract Campaign {
         deadline = _deadline;
         isActive = true; // mark the campaign as active initially
     }
-    
+
     // events
+    // to log donations and notify the beneficiary and the donor on the frontend that the donation has been successfully made
     event DonationMade(
-        address indexed donor, 
-        uint32 amount
-    );
-    event FundsWithdrawn(
+        address indexed campaignAddress,
         address indexed beneficiary,
         uint32 amount
     );
-    event RefundIssued(
-        address indexed donor, 
+
+    // to log fund withdrawals and notify the beneficiary and donors on the frontend that the fudns have been withdrawn from the Campaign
+    event FundsWithdrawn(
+        address indexed campaignAddress,
+        address indexed beneficiary,
         uint32 amount
     );
+
+    // to log refunds and notify the donors on the frontend that refunds have been issued to them
+    event RefundIssued(
+        address indexed campaignAddress,
+        address indexed donor,
+        uint32 amount
+    );
+
+    // to log the finalization of campaigns and notify the beneficiary and donors on the frontend of the Campaign's status; also notifies beneficiary of the refunds if the Campaign failed
     event CampaignFinalized(
         address indexed campaignAddress, // to help the frontend identify the campaigns that have been finalized and those that have not when querying emitted events
-        bool success, 
+        address indexed beneficiary,
+        bool success, // if success is true, it means that the Campaign met its fundingGoal before its deadline
         uint32 totalFunds
-    ); // if success is true, it means that the Campaign met its fundingGoal before its deadline
+    );
 
     // modifiers
     modifier isActiveCampaign() {
@@ -66,35 +77,33 @@ contract Campaign {
     }
 
     modifier deadlineExceeded() {
-        require(block.timestamp > deadline, "The Camapign's deadline has not passed!");
+        require(
+            block.timestamp > deadline,
+            "The Camapign's deadline has not passed!"
+        );
         _;
     }
 
     // functions
-
-    // TODO: implement some kind of rate limiting here as donate() is vulnerable to DDoS attacks
-
-    /** 
+    /**
      * @dev Allows donors to donate to the Campaign
      */
     function donate() external payable isActiveCampaign {
         address donor = msg.sender;
         uint32 amount = uint32(msg.value);
-        
+
         // Add donor to array if this is their first donation
         if (donations[donor] == 0) {
             donors.push(donor);
         }
-        
+
         donations[donor] += amount;
-        
+
         // store as a variable in memory so that we don't keep reading from the state variable, totalFunds, to save on gas fees
         uint32 newTotalFunds = totalFunds + amount;
         totalFunds = newTotalFunds;
-        emit DonationMade(
-            donor, 
-            amount
-        );
+
+        emit DonationMade(address(this), beneficiary, amount);
 
         // if the Campaign has been fully funded, then release all the funds to the beneficiary
         if (newTotalFunds >= fundingGoal) {
@@ -102,7 +111,7 @@ contract Campaign {
         }
     }
 
-    /** 
+    /**
      * @dev Releases the totalFunds to the beneficiary
      * @dev It is called whenever the totalFunds exceed the fundingGoal before or by the deadline
      * @dev It is some sort of a helper function
@@ -113,34 +122,34 @@ contract Campaign {
         isActive = false;
 
         // transfer the totalFunds to beneficiary
-        (bool success,) = payable(beneficiary).call{value: fundsToRelease}("");
+        (bool success, ) = payable(beneficiary).call{value: fundsToRelease}("");
         require(success, "Unable to release funds!");
 
-        emit FundsWithdrawn(
-            beneficiary,
-            fundsToRelease
-        );
+        emit FundsWithdrawn(address(this), beneficiary, fundsToRelease);
         emit CampaignFinalized(
-            address(this), 
-            true, 
+            address(this),
+            beneficiary,
+            true,
             fundsToRelease
         );
     }
 
     // TODO: implement the access control to ensure that only the admin can call this function @carina
-    /** 
+    /**
      * @dev Called when the Campaign's deadline has passed, and also handles the refund to all donors if the fundingGoal is not met by then
      * @dev Likely needs to be called from the frontend
      */
     function finalizeCampaign() external isActiveCampaign deadlineExceeded {
-        if (totalFunds >= fundingGoal) { //SX: Changed > to >=
+        if (totalFunds >= fundingGoal) {
+            //SX: Changed > to >=
             releaseFunds();
         } else {
             isActive = false; // Called here and not before if statement because releaseFunds() has isActive = False as well.
-            
+
             // Refund totalFunds to all donors by calling the refund function for each donor
-            uint totalDonors = donors.length; // Minimising gas used for repeatedly reading the array length inside the loop/
-            for (uint i = 0; i < totalDonors; i++) {
+            uint256 totalDonors = donors.length; // Minimising gas used for repeatedly reading the array length inside the loop
+
+            for (uint256 i = 0; i < totalDonors; i++) {
                 address donor = donors[i];
                 uint32 donationAmount = donations[donor];
                 // Removed the use of `refund()` helper function to avoid gas costs associated with function jumps
@@ -148,38 +157,70 @@ contract Campaign {
                 if (donationAmount > 0) {
                     donations[donor] = 0;
                     payable(donor).transfer(donationAmount);
-                    emit RefundIssued(
-                        donor, 
-                        donationAmount
-                    );
+
+                    emit RefundIssued(address(this), donor, donationAmount);
                 }
-                
             }
-            
+
+            delete donors; // no longer needed, as this state variable is used to keep track of donor addresses
+
             emit CampaignFinalized(
-                address(this), 
-                false, 
+                address(this),
+                beneficiary,
+                false,
                 totalFunds
             );
-
         }
     }
 
-    /** 
-     * @dev Refunds the donor's donation
-     * @dev It is some sort of a helper function
-     * @param donor The donor wallet to refund the donation back to
+    /**
+     * @dev Get all the donors of this Campaign
+     * @dev Likely to be used by the frontend to notify just donors on events such as FundsWithdrawn and CampaignFinalized
      */
-    function refund(address donor) private {
-        uint32 donationAmount = donations[donor];
-        delete donations[donor]; // more gas efficient than donations[donor] = 0
-        
-        // refund to the donor
-        payable(donor).transfer(donationAmount);
-
-        emit RefundIssued(
-            donor, 
-            donationAmount
-        );
+    function getDonors() external view returns (address[] memory) {
+        return donors;
     }
+
+    /**
+     * @dev Get all the Campaign details from its state variables
+     */
+    function getCampaignDetails()
+        external
+        view
+        returns (
+            address campaignBeneficiary,
+            string memory campaignName,
+            string memory campaignDescription,
+            uint256 campaignDeadline,
+            uint32 campaignFundingGoal,
+            uint32 campaignTotalFunds,
+            bool campaignIsActive
+        )
+    {
+        campaignBeneficiary = beneficiary;
+        campaignName = name;
+        campaignDescription = description;
+        campaignDeadline = deadline;
+        campaignFundingGoal = fundingGoal;
+        campaignTotalFunds = totalFunds;
+        campaignIsActive = isActive;
+    }
+
+    // /**
+    //  * @dev Refunds the donor's donation
+    //  * @dev It is some sort of a helper function
+    //  * @param donor The donor wallet to refund the donation back to
+    //  */
+    // function refund(address donor) private {
+    //     uint32 donationAmount = donations[donor];
+    //     delete donations[donor]; // more gas efficient than donations[donor] = 0
+
+    //     // refund to the donor
+    //     payable(donor).transfer(donationAmount);
+
+    //     emit RefundIssued(
+    //         donor,
+    //         donationAmount
+    //     );
+    // }
 }
--- backend/contracts/CampaignFactory.sol
+++ backend/contracts/CampaignFactory.sol
@@ -4,23 +4,23 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "backend/contracts/Campaign.sol";
 
-/** 
+/**
  * @title CampaignFactory
  * @dev Creates and deploy a new Campaign smart contract whenever any beneficiary creates a campaign
  */
 contract CampaignFactory {
     // Creates and deploys a new Campaign smart contract whenever any beneficiary creates a campaign from the frontend
-    
+
     // events
     // logs the creation of a new Campaign
     event CampaignCreated(
         address indexed campaignAddress, // indexed for more efficient event filtering by frontend
         uint32 fundingGoal,
-        uint deadline
+        uint256 deadline
     );
 
     // functions
-    /** 
+    /**
      * @dev Allows beneficiaries to create a Campaign with the required details
      * @param _name Name of the Campaign
      * @param _description Description of the Campaign
@@ -31,7 +31,7 @@ contract CampaignFactory {
         string calldata _name,
         string calldata _description,
         uint32 _fundingGoal,
-        uint _deadline
+        uint256 _deadline
     ) external {
         // create a new campaign smart contract
         Campaign campaign = new Campaign(
@@ -43,14 +43,12 @@ contract CampaignFactory {
         );
 
         // emit CampaignCreated event to allow the frontend to retrieve the Campaign's address by filtering the logs for the CampaignCreated event
-        emit CampaignCreated(
-            address(campaign), 
-            _fundingGoal, 
-            _deadline
-        );
+        emit CampaignCreated(address(campaign), _fundingGoal, _deadline);
     }
 
-    // /** 
+    // TODO: consider adding some functions to let users independently check campaign status here; since we are not storing the Campaign addresses right here in CampaignFactory, there is a risk of partial centralization, as users need to trust the frontend to provide the correct list of deployed active contracts
+
+    // /**
     //  * @dev Returns the list of active deployed Campaigns to the frontend
     //  */
     // function getDeployedCampaigns() external view returns (address[] memory) {
